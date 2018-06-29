6015932defdee5b8634d9fc04f4af2278dd3fc96
TheMelonOne <paichun.wang@yahoo.com>
2018-06-28 22:32:07 -0500

    pokemon game update

--- RPG_Game/assets/javascript/game.js
+++ RPG_Game/assets/javascript/game.js
@@ -1,62 +1,115 @@
 $(document).ready(function () {
 
+    var charHP = 0;
+    var charAttk = 0;
+    var charDef = 0;
+
     var pokemonChoice = [
         "../RPG_Game/assets/images/pokeball.png",
         "../RPG_Game/assets/images/001.png",
         "../RPG_Game/assets/images/004.png",
-        "../RPG_Game/assets/images/007.png",
-        "../RPG_Game/assets/images/010.png",
-        "../RPG_Game/assets/images/019.png"]
+        "../RPG_Game/assets/images/007.png",]
 
-     $("#1").mouseover(function () {
+    var pokemonStats = {
+        "Bulbasaur": {
+            "Health": "45",
+            "Attack": "49",
+            "Defence": "49"
+        },
+
+        "Charmander": {
+            "Health": "39",
+            "Attack": "52",
+            "Defence": "43"
+        },
+
+        "Squirtle": {
+            "Health": "44",
+            "Attack": "48",
+            "Defence": "65"
+        }
+    }
+
+    $("#1").mouseover(function () {
         this.src = pokemonChoice[1]
         $("#chosen").text("Bulbasaur");
         $("#1").on('click', function () {
             console.log("Bulbasaur");
-            $("#pokemon img").attr('src',pokemonChoice[0]);
+            $("#pokemon img").attr('src', pokemonChoice[0]);
             $(".clickElement").off('click');
-            $("#1").attr('src',pokemonChoice[1]);
+            $("#1").attr('src', pokemonChoice[1]);
             $(".clickElement").off('mouseover');
             $(".clickElement").off('mouseout');
-       });
-     }).mouseout(function () {
-         this.src= "../RPG_Game/assets/images/pokeball.png"
-         $("#chosen").text("");
-     });
 
-     $("#4").mouseover(function () {
+            charHP = pokemonStats.Bulbasaur.Health;
+            charAttk = pokemonStats.Bulbasaur.Attack;
+            charDef = pokemonStats.Bulbasaur.Defence;
+
+            gameStart();
+        });
+    }).mouseout(function () {
+        this.src = "../RPG_Game/assets/images/pokeball.png"
+        $("#chosen").text("");
+    });
+
+    $("#4").mouseover(function () {
         this.src = pokemonChoice[2]
         $("#chosen").text("Charmander");
         $("#4").on('click', function () {
             console.log("Charmander");
-            $("#pokemon img").attr('src',pokemonChoice[0]);
+            $("#pokemon img").attr('src', pokemonChoice[0]);
             $(".clickElement").off('click');
-            $("#4").attr('src',pokemonChoice[2]);
+            $("#4").attr('src', pokemonChoice[2]);
             $(".clickElement").off('mouseover');
             $(".clickElement").off('mouseout');
-        });
-     }).mouseout(function () {
-         this.src= "../RPG_Game/assets/images/pokeball.png"
-         $("#chosen").text("");
-     });
 
-     $("#7").mouseover(function () {
+            charHP = pokemonStats.Charmander.Health;
+            charAttk = pokemonStats.Charmander.Attack;
+            charDef = pokemonStats.Charmander.Defence;
+
+            gameStart();
+        });
+    }).mouseout(function () {
+        this.src = "../RPG_Game/assets/images/pokeball.png"
+        $("#chosen").text("");
+    });
+
+    $("#7").mouseover(function () {
         this.src = pokemonChoice[3]
         $("#chosen").text("Squirtle");
         $("#7").on('click', function () {
             console.log("Squirtle");
-            $("#pokemon img").attr('src',pokemonChoice[0]);
+            $("#pokemon img").attr('src', pokemonChoice[0]);
             $(".clickElement").off('click');
-            $("#7").attr('src',pokemonChoice[3]);
+            $("#7").attr('src', pokemonChoice[3]);
             $(".clickElement").off('mouseover');
             $(".clickElement").off('mouseout');
-        });
-     }).mouseout(function () {
-         this.src= "../RPG_Game/assets/images/pokeball.png"
-         $("#chosen").text("");
-     });
-});
 
-function gameStart() {
-    
-}
+            charHP = pokemonStats.Squirtle.Health;
+            charAttk = pokemonStats.Squirtle.Attack;
+            charDef = pokemonStats.Squirtle.Defence;
+
+            gameStart();
+        });
+    }).mouseout(function () {
+        this.src = "../RPG_Game/assets/images/pokeball.png"
+        $("#chosen").text("");
+    });
+
+    function gameStart() {
+        $("#enemy").css("visibility", "visible")
+        enemy();
+        $("#action").css("visibility", "visible")
+        action();
+        console.log("Health:" + charHP + " " + "Attack:" + charAttk + " " + "Defence:" + charDef);
+    }
+
+    function enemy() {
+        var enemyChoice = Math.floor(Math.random() * 3 + 1);
+        $("#enemy img").attr('src', pokemonChoice[enemyChoice]);
+    }
+
+    function action() {
+
+    }
+});
