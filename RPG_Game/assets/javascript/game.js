cafbdd76c54fcbfab0787b12e7bb5f068e7bdb1e
TheMelonOne <paichun.wang@yahoo.com>
2018-07-03 18:05:59 -0500

    Updating Homework

--- RPG_Game/assets/javascript/game.js
+++ RPG_Game/assets/javascript/game.js
@@ -112,4 +112,19 @@ $(document).ready(function () {
     function action() {
 
     }
+
+    $(function () {
+        $(".playback").click(function (e) {
+            e.preventDefault();
+
+            // This next line will get the audio element
+            // that is adjacent to the link that was clicked.
+            var song = $(this).next('audio').get(0);
+            if (song.paused)
+                song.play();
+            else
+                song.pause();
+        });
+    });
+    
 });
